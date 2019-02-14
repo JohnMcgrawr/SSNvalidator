c627ab361c79872e5470bd3f7c2c2aee438ca090
JohnMcGrawr <john_r_lindstrom@outlook.com>
2019-02-14 21:39:22 +0100

    inor fix

--- src/main/java/se/jrl/validator/ValidatorController.java
+++ src/main/java/se/jrl/validator/ValidatorController.java
@@ -5,22 +5,23 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ValidatorController {
 
-	@RequestMapping(value = "/validate/{data}")
+	@RequestMapping(value = ("/validate/{data}"), method = RequestMethod.GET)
 	public ResponseEntity<String> getResource(@PathVariable String data) {
 		return Utils.validateCCNumber(data);
 	}
 
-	@RequestMapping(value = "/validate")
+	@RequestMapping(value = ("/validate"), method = RequestMethod.GET)
 	public ResponseEntity<String> isNull() {
 		return ResponseEntity.badRequest().body("SSN can not be null");
 	}
 
-	@RequestMapping(value = "/getInvalidSSNList")
+	@RequestMapping(value = ("/getInvalidSSNList"), method = RequestMethod.GET)
 	public List<String> getIncorrectSSNList() {
 		return Utils.getInvalidSSNList();
 	}
